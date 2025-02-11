11923cb3f34078be1c31b9d694145557af9bdc03
novmasha <novmasha@bk.ru>
2025-02-11 13:03:58 +0300

    итоговая версия

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -12,25 +12,13 @@ namespace WebApplication2
         static async Task Main(string[] args)
         {
             using IHost host = Host.CreateDefaultBuilder(args) //метод создает и конфигурирует хост приложения
-                .ConfigureAppConfiguration((hostingContext, configuration) =>
-                //Host.CreateDefaultBuilder(args): Создает стандартный хост-билдер, который автоматически
-                //загружает конфигурацию из переменных окружения и аргументов командной строки
-                //.ConfigureAppConfiguration((hostingContext, configuration) => { ... }):  Позволяет настроить источники конфигурации
-                //в данном случае, он добавляет переменные окружения и аргументы командной строки
-                {
-                    // Конфигурация приложения
-                    configuration.Sources.Clear();
-                    configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-                    configuration.AddEnvironmentVariables();
-                    configuration.AddCommandLine(args);
-                })
                 //регистрируем наши сервисы в контейнере DI
                 .ConfigureServices((hostContext, services) =>
                 {
 
                     services.AddSingleton<IMessageService, MessageService>(); // Регистрирует MessageService как реализацию интерфейса IMessageService
                                                                               // AddSingleton означает, что будет создан только один экземпляр MessageService на все время жизни приложения
-                    services.AddTransient<MyApplication>();  //Регистрируем MyApplication как transient, это нужно, чтобы можно было получить экземпляр MyApplication из контейнера D
+                    services.AddTransient<MyApplication>();  //Регистрируем MyApplication как transient, это нужно, чтобы можно было получить экземпляр MyApplication из контейнера DI
                 })
                 .Build(); //Строит хост приложения
 
